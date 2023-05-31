5846ac7b8ec59420ca54a3b496a6cbbb2f318d5c
voronovski <avoronovski@gmail.com>
2023-05-31 00:55:16 -0700

    main logic on SwiftUI

--- SliderAppearanceGame/ContentView.swift
+++ SliderAppearanceGame/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var targetValue = Double.random(in: 0...100).rounded()
+    @State private var currentValue = Double.random(in: 0...100).rounded()
+    @State private var showingAlert = false
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Move slider closer to: \(targetValue.formatted())")
+            Slider(value: $currentValue, in: 0...100, step: 1)
+                .accentColor(
+                    Color
+                        .blue
+                        .opacity(Double(100 - abs(currentValue - targetValue))/100)
+                )
+            Button("Check") {
+                score = computeScore()
+                showingAlert = true
+            }
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Your Score"),
+                      message: Text("\(score)"),
+                      dismissButton: .default(Text("OK")))
+            }
+            
+            Button("Start over") {
+                startOver()
+            }
         }
         .padding()
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - currentValue)
+        return lround(100 - difference)
+    }
+    
+    private func startOver() {
+        targetValue = Double.random(in: 0...100).rounded()
+        currentValue = 0
     }
 }
 
