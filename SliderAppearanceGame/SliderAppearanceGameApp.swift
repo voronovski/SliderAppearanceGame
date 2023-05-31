633edb134c17970e72a1760f9c8f15dc79235799
voronovski <avoronovski@gmail.com>
2023-05-30 18:30:26 -0700

    Initial Commit

---
+++ SliderAppearanceGame/SliderAppearanceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppearanceGameApp.swift
+//  SliderAppearanceGame
+//
+//  Created by Aleksei Voronovskii on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppearanceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
