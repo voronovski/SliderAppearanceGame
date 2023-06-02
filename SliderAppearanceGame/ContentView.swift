e587ba0150c715bf0a09aa4b8e2811414ab7eaa8
voronovski <avoronovski@gmail.com>
2023-06-02 14:36:16 -0700

    code improvements

--- SliderAppearanceGame/ContentView.swift
+++ SliderAppearanceGame/ContentView.swift
@@ -8,43 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var targetValue = Double.random(in: 0...100).rounded()
-    @State private var currentValue = Double.random(in: 0...100).rounded()
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var currentValue = Double.random(in: 0...100)
     @State private var showingAlert = false
-    @State private var score = 0
+    
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Move slider closer to: \(targetValue.formatted())")
-            CustomSlider(
-                targetValue: $targetValue,
-                value: $currentValue)
+            Text("Move slider closer to: \(targetValue)")
+            HStack {
+                Text("0")
+                CustomSliderView(
+                    value: $currentValue,
+                    alpha: computeScore()
+                )
+                Text("100")
+            }
             
             Button("Check") {
-                score = computeScore()
-                showingAlert = true
+                showingAlert.toggle()
             }
             .alert(isPresented: $showingAlert) {
                 Alert(title: Text("Your Score"),
-                      message: Text("\(score)"),
+                      message: Text(computeScore().formatted()),
                       dismissButton: .default(Text("OK")))
             }
             
             Button("Start over") {
-                startOver()
+                targetValue = Int.random(in: 0...100)
+                currentValue = Double.random(in: 0...100)
             }
         }
         .padding()
     }
     
     private func computeScore() -> Int {
-        let difference = abs(targetValue - currentValue)
-        return lround(100 - difference)
-    }
-    
-    private func startOver() {
-        targetValue = Double.random(in: 0...100).rounded()
-        currentValue = 0
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
--- SliderAppearanceGame/CustomSliderView.swift
+++ SliderAppearanceGame/CustomSliderView.swift
@@ -1,5 +1,5 @@
 //
-//  CustomSlider.swift
+//  CustomSliderView.swift
 //  SliderAppearanceGame
 //
 //  Created by Aleksei Voronovskii on 6/1/23.
@@ -7,9 +7,10 @@
 
 import SwiftUI
 
-struct CustomSlider: UIViewRepresentable {
-    @Binding var targetValue: Double
+struct CustomSliderView: UIViewRepresentable {
     @Binding var value: Double
+    
+    let alpha: Int
     
     func makeUIView(context: Context) -> UISlider {
         let slider = UISlider()
@@ -27,34 +28,30 @@ struct CustomSlider: UIViewRepresentable {
     
     func updateUIView(_ uiView: UISlider, context: Context) {
         uiView.value = Float(value)
-        let alpha = Double(100 - abs(value - targetValue))/100
-        uiView.thumbTintColor = UIColor(
-            red: 1,
-            green: 0,
-            blue: 0,
-            alpha: alpha
-        )
+        uiView.thumbTintColor = .red.withAlphaComponent(CGFloat(alpha) / 100)
     }
     
     func makeCoordinator() -> Coordinator {
-        Coordinator(self)
+        Coordinator(value: $value)
     }
-    
+}
+
+extension CustomSliderView {
     class Coordinator: NSObject {
-        var parent: CustomSlider
+        @Binding var value: Double
                 
-        init(_ parent: CustomSlider) {
-            self.parent = parent
+        init(value: Binding<Double>) {
+            self._value = value
         }
         
         @objc func valueChanged(_ sender: UISlider) {
-            parent.value = Double(sender.value)
+            value = Double(sender.value)
         }
     }
 }
 
 struct CustomSlider_Previews: PreviewProvider {
     static var previews: some View {
-        CustomSlider(targetValue: .constant(50.0), value: .constant(50.0))
+        CustomSliderView(value: .constant(50.0), alpha: 100)
     }
 }
